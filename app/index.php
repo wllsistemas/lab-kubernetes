14a5f03f98260353d310bf7d6c41c7a507a4c3e7
WILLIAM FRANCISCO LEITE <william@wllsistemas.com.br>
2025-10-07 11:28:37 -0300

    commit inicial

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KUBERNETES</title>
+</head>
+
+<body>
+    <h1>KUBERNETES LAB <?= date('Y-m-d H:i:s') ?></h1>
+    <?php
+    echo "<p>APP_ENV: " . getenv('KUBERNETES_APP_ENV') . "</p>";
+    echo "<p>DB_HOST: " . getenv('KUBERNETES_DB_HOST') . "</p>";
+    echo "<p>DB_PASSWORD_SECRET: " . getenv('DATABASE_PASSWORD') . "</p>";
+    echo "<p>API_KEY_SECRET: " . getenv('EXTERNAL_API_KEY') . "</p>";
+    ?>
+</body>
+
+</html>
